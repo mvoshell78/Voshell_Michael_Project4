25a3c8c86d9acb77fbff4ef902f6b950fc166034
mvoshell78 <michael.voshell@yahoo.com>
2014-09-22 18:32:31 -0400

    finished telephone verify function
    refactored code removed console logs I was using to follow the code

--- Voshell_Michael_Project4.js
+++ Voshell_Michael_Project4.js
@@ -7,49 +7,34 @@
 
 // Functions
 
+// This function verifies is a string of data is a phone number 
 var verifyPhone = function () {
 var phonePrompt = prompt( " What is your phone number? Use 111-111-1111 format ")
-console.log (phonePrompt)
+console.log (" You entered " + phonePrompt )
 var areaCode = (phonePrompt.substr(0,3))
 var prefix = (phonePrompt.substr(4,3))
 var lastFour= (phonePrompt.substr(8,4))
 var wholeNumber = parseInt(areaCode + prefix + lastFour)
 
     if (phonePrompt.length != 12) {
-     console.log ("this does not fit the 111-111-1111 format ")
      return false
-    
-        } else {
-            
-            console.log ("it's 12 digits")
-            
+        } else {   
         if (phonePrompt.substr(3,1) != "-") {
-            console.log ("this does not fit the 111-111-1111 format ")
             return false
-        
                 } else {
-                    
-                    console.log (" has 1st dash ")
-                    
                     if (phonePrompt.substr(7,1) != "-") {
-                    console.log ("this does not fit the 111-111-1111 format ")
-                    
-                     return false
-                    
+                        return false
                     } else {
-                        console.log ("it has a second dash")
-                        if (wholeNumber >= 9999999999) {
-                            if (wholeNumber <= 1111111111) {
-                                console.log ("this number is not between 1 and 9")
+                        if (wholeNumber <= 9999999999) {
+                            if (wholeNumber >= 1111111111) {
+                                return true
+                            } else {
                                 return false
-                            } else { 
-                                
-                                return true  
+                            };
                         };
                         
                     };
 
-};
 };
 };
 };
@@ -58,6 +43,13 @@ var wholeNumber = parseInt(areaCode + prefix + lastFour)
 
 // Main Code
 
-var phone = verifyPhone()
-console.log (phone)
+var phone = verifyPhone() // conditional statements for return
+if (phone === true) {
+    console.log ("This Telephone number fits the format" )
+    } else {
+        if (phone === false) {
+            console.log ("This is not a phone number or it does not for the format ")
+            }   ;// false conditional
+    }; // close conditonal
+
 
