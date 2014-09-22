53906e7d7525a1fa396972f7e19df04ff4a8f5a2
mvoshell78 <michael.voshell@yahoo.com>
2014-09-22 15:45:00 -0400

    initial setup
    initial setup 1st function

---
+++ Voshell_Michael_Project4.js
@@ -0,0 +1,29 @@
+//alert("JavaScript works!");
+// Michael Voshell
+// SDI 092014
+// Project 4
+
+// Global Variables
+
+// Functions
+
+var phonePrompt = prompt( " What is your phone number? in a 111-111-1111 format ")
+
+if (phonePrompt.length < 12) {
+    console.log ("this does not fit the 111-111-1111 format ")
+    } else {
+    var i = 0
+    var phoneDigit = 0
+    while (i != -1) {
+        var phoneDigit = phonePrompt.substr()
+        console.log (phoneDigit)
+        if (i != -1) {
+         i++
+        }
+       
+        
+    };
+
+};
+
+// Main Code
