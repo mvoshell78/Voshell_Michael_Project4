96aaefd84f47c4dc5536afdcf26336e96514a2c2
mvoshell78 <michael.voshell@yahoo.com>
2014-09-22 17:17:37 -0400

    working on telephone pattern
    not working

--- Voshell_Michael_Project4.js
+++ Voshell_Michael_Project4.js
@@ -7,23 +7,51 @@
 
 // Functions
 
-var phonePrompt = prompt( " What is your phone number? in a 111-111-1111 format ")
-
-if (phonePrompt.length < 12) {
-    console.log ("this does not fit the 111-111-1111 format ")
-    } else {
-    var i = 0
-    var phoneDigit = 0
-    while (i != -1) {
-        var phoneDigit = phonePrompt.substr()
-        console.log (phoneDigit)
-        if (i != -1) {
-         i++
-        }
-       
-        
-    };
-
+var verifyPhone = function () {
+var phonePrompt = prompt( " What is your phone number? Use 111-111-1111 format ")
+console.log (phonePrompt)
+    if (phonePrompt.length != 12) {
+     console.log ("this does not fit the 111-111-1111 format ")
+     return false
+        } else {
+            console.log ("it's 12 digits")
+        if (phonePrompt.substr(3,1) != "-") {
+            console.log ("this does not fit the 111-111-1111 format ")
+            return false
+                } else {
+                    console.log (" has 1st dash ")
+                    if (phonePrompt.substr(7,1) != "-") {
+                    console.log ("this does not fit the 111-111-1111 format ")
+                     return false
+                        } else {
+                            console.log("has 2nd dash ")      
+                            if (parseInt(phonePrompt) > 111) {
+                                if (parseInt(phonePrompt) < 999) {
+                            console.log (" I am between 111 and 999")
+                                };
+                                };
+                                var prefix = (parseInt(phonePrompt.substr(4,3)) {
+                                    console.log(prefix)
+                              
+                            if  (prefix < 111) {
+                                if (prefix > 999) {
+                            console.log (" I am between 111 and 999")
+                                };
+                                };
+                                };
+                            if (phonePrompt.substr(8,4) < 1111) {
+                                if (phonePrompt.substr(8,4) > 9999) {
+                             console.log (" I am between 1111 and 9999")
+                                 return true
+                                }; 
+                                };
+                        };
+                };
+        };
 };
 
 // Main Code
+
+var phone = verifyPhone()
+console.log (phone)
+
