0694e1f055458a9eb7b9788a2cec771d3ac67a1f
mvoshell78 <michael.voshell@yahoo.com>
2014-09-22 21:24:27 -0400

    added difference between two dates function
    the difference between now and Christmas

--- Voshell_Michael_Project4.js
+++ Voshell_Michael_Project4.js
@@ -5,12 +5,15 @@
 
 // Global Variables
 
+var dateCurrent = new Date(); 
+var dateFuture = new Date("2014/12/25")  
+
 // Functions
 
 // Does a string follow a 123-456-7890 pattern like a phone number?
 var verifyPhone = function () {
 
-console.log (" You entered " + phonePrompt )
+
 var areaCode = (phonePrompt.substr(0,3))
 var prefix = (phonePrompt.substr(4,3))
 var lastFour= (phonePrompt.substr(8,4))
@@ -43,7 +46,7 @@ var wholeNumber = parseInt(areaCode + prefix + lastFour)
     
 var webAddress = function() {
     
-    console.log(webPrompt)
+    
     if (webPrompt.substr(0,5) === "http:") {
         return true 
     } else {
@@ -55,11 +58,32 @@ var webAddress = function() {
     };//2nd coditional
    };// end of webAddress function
  
+// Find the number of hours or days difference between two dates.
 
+var dateDiff = function(dateCurrent, dateFuture) {
+    
+    var diffInHours = Math.abs((((dateCurrent-dateFuture)/1000)/60)/60); // calculates hours
+    var diffInDays = Math.abs(((((dateCurrent-dateFuture)/1000)/60)/60)/24); //  calculates days
+    diffInHours = Math.round(diffInHours);
+    diffInDays = Math.round(diffInDays);
+    return [diffInDays, diffInHours]    // returning an array
+    };
 
 // Main Code
 
+
+// calling the diffdate function
+
+var christmas = dateDiff(dateCurrent,dateFuture)
+    var christmasDays = christmas[0]
+    var christmasHours = christmas[1]
+        console.log (christmasDays + " days left till Christmas")
+        console.log ("Better get with it, thats only " + christmasHours + " more hours!")
+
+// calling verify phone 
+
 var phonePrompt = prompt( " What is your phone number? Use 111-111-1111 format ")
+console.log (" You entered " + phonePrompt )
 var phone = verifyPhone(phonePrompt) // conditional statements for return
 if (phone === true) {
     console.log ("This Telephone number fits the format" )
@@ -69,9 +93,11 @@ if (phone === true) {
             }   ;// false conditional
     }; // close conditonal*/
 
+// verify url
+
 var webPrompt = prompt ("Enter a full web address, including https:")   
 var web = webAddress()
-
+console.log(webPrompt)
 if (web === true) {
     console.log("This is a url")
     } else {
